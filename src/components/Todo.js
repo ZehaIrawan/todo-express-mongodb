2087b7960cdf16e6d102b86d0c5da5b768190d37
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-04 10:34:04 +0700

    todo form mockup

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const Todo = () => {
+  const [formData, setFormData] = useState({
+    todo: '',
+  });
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const { todo } = formData;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
   return (
     <div>
-      <h1>Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="todo" id="todoLabel">
+          <input
+            type="text"
+            name="todo"
+            id="todo"
+            onChange={e => onChange(e)}
+            value={todo}
+            placeholder="Enter a task"
+            required
+          />
+        </label>
+        <button type="submit">Add Task</button>
+      </form>
     </div>
-  )
-}
+  );
+};
