75e0e78aa828a7347247ac493a1081775c607f9d
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-04 10:18:50 +0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Todo} from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
