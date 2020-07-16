a419b38c3929e8e5e62f15549c830967426dc331
Zeha Irawan <jangkaringbumi@gmail.com>
2020-07-17 05:45:47 +0700

    add routes for user & todo

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/todos', require('./routes/api/todos'))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
