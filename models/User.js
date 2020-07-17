879cd42d9feeaa8954b85bcd06bb94c3e98340c9
Zeha Irawan <jangkaringbumi@gmail.com>
2020-07-17 08:36:54 +0700

    implement User registration

--- models/User.js
+++ models/User.js
@@ -5,6 +5,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   password: {
     type: String,
     required: true,
